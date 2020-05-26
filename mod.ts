d2dc4f3646ccaf491a279a4493acefd210acc9de
Damian Perera <damianperera@outlook.com>
2020-05-26 16:15:48 +0530

    generated typedoc html

--- mod.ts
+++ mod.ts
@@ -1,7 +1,9 @@
 /**
- * Extends Array.Prototype with the below functions.
- * 
  * @author Damian Perera
+ */
+
+/**
+ * Extends Array.Prototype with the below functions.
  */
 export namespace Arrays {
     
@@ -243,17 +245,174 @@ export namespace Arrays {
 }
 
 declare global {
+
+    /**
+     * Extends [Array.Prototype](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Instance_methods) with the below methods.
+     */
     interface Array<T> {
+        /**
+         * Returns an array split into chunks. If the array can't be split equally based on the given size, the last chunk will be the remaining elements.
+         *
+         *     import 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr = [1, 2, 3, 4, 5]
+         *     const chunkedArr = arr.chunk(2)
+         *     // => [[1, 2], [3, 4], 5]
+         * 
+         * @param {number} size - Size of each chunk
+         * 
+         */
         chunk(size: number): Array<any>
+
+        /**
+         * Returns an array with all falsey values (false, null, 0, '', undefined, and NaN) removed.
+         *
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr = [1, 2, 3, 4, NaN, 5, null, 6]
+         *     const compactArr = arr.compact()
+         *     // => [1, 2, 3, 4, 5, 6]
+         * 
+         */
         compact(): Array<any>
+
+        /**
+         * Merge the elements from the rest of the arrays (args) to the first array. A nested array will be considered as a single value.
+         *
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr1 = [1, 2, 3, 4]
+         *     const arr2 = ['dog', 'cat']
+         *     const arr3 = [1.1, 2.2, 3.3]
+         *     const arr4 = [[ 'cheetah', 'rhino' ], 'monkey']
+         * 
+         *     const mergedArr = arr1.merge(arr2, arr3, arr4)
+         *     // => [1, 2, 3, 4, 'dog', 'cat', 1.1, 2.2, 3.3, ['cheetah', 'rhino'], 'monkey']
+         * 
+         * @param {Array} args - Rest of the arrays
+         */
         merge(...args: Array<any>): Array<any>
+
+        /**
+         * Returns the unique values from a given array. Optionally pass in a boolean to specify if the result should be sorted.
+         *
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr = [1, 1, 'Dog', 'Dog', 123.42, 123.42]
+         *     const uniqueArr = arr.unique()
+         *     // => [1, 'Dog', 123.42]
+         * 
+         * @param {Boolean} sort - Return sorted values, defaults to false
+         */
         unique(sort?: Boolean): Array<any>
+
+        /**
+         * Returns the common values from all the given arrays.
+         *
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr1 = [1, 2, 3, 4]
+         *     const arr2 = [2, 'cat']
+         *     const arr3 = [1.1, 2.2, 3]
+         *     const arr4 = [[ 'cheetah', 'rhino' ], 4]
+         * 
+         *     const commonArr = arr1.common(arr2, arr3, arr4)
+         *     // => [2, 3, 4]
+         * 
+         * @param {Array} args - Rest of the arrays
+         */
         common(...args: Array<any>): Array<any>
+
+        /**
+         * Returns the unique elements in an array compared to the rest of the arrays (args).
+         *
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr1 = [1, 2, 3, 4]
+         *     const arr2 = [2, 'cat']
+         *     const arr3 = [1.1, 2.2, 3]
+         *     const arr4 = [[ 'cheetah', 'rhino' ], 4]
+         * 
+         *     const diffArr = arr1.diff(arr2, arr3, arr4)
+         *     // => [1]
+         * 
+         * @param {Array} args - Rest of the arrays
+         */
         diff(...args: Array<any>): Array<any>
+
+        /**
+         * Returns an array without the values passed as args.
+         *
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr = [1, 1, 'Dog', 'Dog', 123.42, 123.42]
+         *     const res = arr.remove('Dog')
+         *     // => [1, 1, 123.42, 123.42]
+         * 
+         *     const res2 = arr.remove(1, 'Dog')
+         *     // => [123.42, 123.42]
+         * 
+         * @param {any} args - Elements to remove
+         */
         remove(...args: any): Array<any>
+
+        /**
+         * Flattens an arrays of arrays into a single array.
+         * 
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr = [[ 'cheetah', 'rhino', ['sun', 'moon'], [['nested nested', 'test']]], 4]
+         *     const res = arr.flatten()
+         *     // => ['cheetah', 'rhino', 'sun', 'moon', 'nested nested', 'test', 4]
+         * 
+         */
         flatten(): Array<any>
+
+        /**
+         * Checks if all the elements in the source array are present in the other arrays (args).
+         *
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr1 = [1, 2, 3, 4]
+         *     const arr2 = [2, 'cat']
+         *     const arr3 = [1.1, 2.2, 3]
+         *     const arr4 = [[ 'cheetah', 'rhino' ], 4]
+         * 
+         *     const arr5 = ['deno', 'land']
+         *     const arr6 = ['land', 'deno']
+         * 
+         *     const falseyArr = arr1.containsAll(arr1, arr2, arr3, arr4)
+         *     // => false
+         * 
+         *     const truthyArr = arr5.containsAll(arr6)
+         *     // => true
+         * 
+         * @param {Array} args - Rest of the arrays
+         */
         containsAll(...args: Array<any>): Boolean
+
+        /**
+         * Converts the nested arrays of an array into a single object of key-value pairs.
+         * 
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr = [['name', 'deno'], ['location', 'land']].toObject()
+         *     // => { 'name': 'deno', 'location': 'land' }
+         * 
+         */
         toObject(): Object
+
+        /**
+         * Flattens an array and checks if all the elements are of the specified type.
+         * 
+         *     import from 'https://deno.land/x/arrays/mod.ts'
+         * 
+         *     const arr = [['dog', ['sun', 'sky'], 'moon'], 'test']
+         *     const isValid = arr.isType('number')
+         *     // => false
+         * 
+         * @param {string} type - Type to match
+         */
         isType(value: string): Boolean
     }
 }
